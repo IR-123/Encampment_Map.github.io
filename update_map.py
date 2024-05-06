689ff5c5a158012aac44a19dc7461b1c3f4bc457
ch777868 <123615168+ch777868@users.noreply.github.com>
2024-05-06 18:49:37 -0400

    Update update_map.py

--- update_map.py
+++ update_map.py
@@ -134,7 +134,7 @@ def process_data():
     # Button to turn scroll zoom on/off
     folium.plugins.ScrollZoomToggler().add_to(usa_map)
 
-    # Botton for fullscreen mode
+    # Button for fullscreen mode
     folium.plugins.Fullscreen(
         position="topright",
         title="Expand me",
